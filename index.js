4e9a1e6dc95850e607d5655f61319007408771cf
csferra <168652816+csferra@users.noreply.github.com>
2024-05-08 09:15:41 +0100

    Add basic search engine (no api)

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+  let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
-
-  cityElement.innerHTML = searchInput.value;
+  cityElement.innerHTML = searchInputElement.value;
 }
 
-let currentDate = document.querySelector("#current-date");
-let currentTime = new Date();
-let minutes = currentTime.getMinutes();
-let hours = currentTime.getHours();
-let day = currentTime.getDay();
-
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let formattedDay = days[day];
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-currentDate.innerHTML = `${formattedDay} ${hours}:${minutes}`;
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
