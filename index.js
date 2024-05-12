533d0250be4008c7439a9979e3a7fb5165dd3df2
csferra <168652816+csferra@users.noreply.github.com>
2024-05-12 15:04:55 +0100

    worked on the CSS to change the appearance

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" /> `;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -57,30 +59,75 @@ function handleSearchSubmit(event) {
   searchCity(searchInputElement.value);
 }
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+//function displayFahrenheitTemperature(event) {
+//event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature");
+
+//celsiusLink.classList.remove("active");
+//fahrenheitLink.classList.add("active");
+// let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+//temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+//}
+
+//function displayCelsiusTemperature(event) {
+//event.preventDefault();
+//celsiusLink.classList.add("active");
+//fahrenheitLink.classList.remove("active");
+//let temperatureElement = document.querySelector("#temperature");
+//temperatureElement.innerHTML = Math.round(celsiusTemperature);
+//}
+
+//let celsiusTemperature = null;
+
+function getForecast(city) {
+  let apiKey = "24b318bfc608a94473fe2oc5at4fa2cb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
         <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon"></div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon">
+        <img src="${day.condition.icon_url}"/>
+        </div>
         <div class="weather-forecast-temperatures">
-        <div class="weather-forecast temperature"><strong>15&deg</strong></div>
-        <div class="weather-forecast-temperature">9&deg</div>
+        <div class="weather-forecast temperature"><strong>${Math.round(
+          day.temperature.maximum
+        )}&deg</strong></div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}&deg</div>
         </div>
         </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Santarem");
-displayForecast();
